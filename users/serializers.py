90ef0bb901e907fc7680a8145703d2166445627d
Kelvin Mukotso <kelvinmukotso@gmail.com>
2024-07-14 23:55:09 +0300

    feat:Fetch all Users API

--- users/serializers.py
+++ users/serializers.py
@@ -5,19 +5,21 @@ from django.contrib.auth.password_validation import validate_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email','first_name','last_name')
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password','first_name','last_name')
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
         )
         return user
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register, login
+from .views import register, login, get_all_users
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
+    path('all/', get_all_users, name='get_all_users'),
 ]
